7908af42646c63dbf68f43e1652561c22d861768
gabmnlx <mongabriellagustan03@gmail.com>
2021-03-11 19:07:06 +0800

    bulk commit

--- Bujo/Bujo/settings.py
+++ Bujo/Bujo/settings.py
@@ -74,19 +74,19 @@ WSGI_APPLICATION = 'Bujo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
--- Bujo/todo/views.py
+++ Bujo/todo/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Name
 
-# Create your views here.
+def home(request):
+    if request.method == "POST":
+        user = Name(name=request.POST['name'])
+        user.save()
+        return render(request, 'home.html', {'name':user.name})
+
+    elif Name.objects.all().exists():
+        return render(request, 'home.html',{'name':Name.objects.first().name})
+
+    else: 
+        return render(request, 'home.html')
+
+
+# def name(request):
+#     name = request.GET['name']
+#     return render(request, 'home.html',{'name':name})
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def week(request):
+    return render(request, 'week.html')
+
+def key(request):
+    return render(request, 'key.html')
